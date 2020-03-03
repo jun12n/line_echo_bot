87bce230c82372ba4a3c12d586cac8d37d337cc9
J <abc.jun.321@gmail.com>
2020-03-03 18:49:15 +0900

    modify app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, abort
 import os
 import sys
+from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookParser
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
@@ -10,15 +10,13 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-from weather_forecast import get_day5_data
-
 
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
 # get channel_secret and channel_access_token from your environment variable
@@ -33,47 +31,47 @@ if channel_access_token is None:
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+handler = WebhookHandler(channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-        send_text(event)
 
     return 'OK'
 
 
-def send_text(event):
-    # if weather is true
-    if event.message.text == 'weather':
-        weather_txt = get_day5_data()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=type(weather_txt))
-        )
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=type(event.message.text))
-        )
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    # メッセージでもテキストの場合はオウム返しする
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='aaaaa')
+    )
+# def send_text(event):
+#     # if weather is true
+#     if event.message.text == 'weather':
+#         weather_txt = get_day5_data()
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text=type(weather_txt))
+#         )
+#     else:
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text=event.message.text)
+#         )
 
 
 if __name__ == '__main__':
